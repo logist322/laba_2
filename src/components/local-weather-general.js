4ced64ae225ee6ac37a208c9edb205e11cb752e5
silentPartner <hytornoy-99@mail.ru>
2020-10-28 19:31:19 +0300

    fix

--- src/components/local-weather-general.js
+++ src/components/local-weather-general.js
@@ -6,7 +6,7 @@ const createLocalWeatherGeneralTemplate = ({name, icon, description, temp}) => {
       <h3 class="local-weather__city">${name}</h3>
       <p class="local-weather__short-info">
         <span class="local-weather__icon-wrap">
-          <img class="local-weather__icon" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}" width="75" height="75">
+          <img class="local-weather__icon" src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}" width="75" height="75">
         </span>
         <span class="local-weather__degrees">${temp}&deg;C</span>
       </p>
